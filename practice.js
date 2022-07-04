b59dbbb32d813f8814d90eea3e50709be97d4207
aakriti-negi <96302029+aakriti-negi@users.noreply.github.com>
2022-07-04 19:26:23 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,10 @@
 const express = require('express') // importing express
 const app = express();
 
+// Middleware functions are functions that have access to the request object ( req ), 
+//the response object ( res ), and the next middleware function in the application's request-response cycle. 
+//The next middleware function is commonly denoted by a variable named next .
+
 //creating first middleware
 const middleware1 = function(req,res,next){ 
     console.log('middleWare 1 ');
